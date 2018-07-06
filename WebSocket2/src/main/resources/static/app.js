8e3f610f32f22b3d39a165b99d8e735f0dc9b43c
yqbjtu <yqbjtu@163.com>
2018-07-07 00:12:53 +0800

    websocket2 app.js added

---
+++ WebSocket2/src/main/resources/static/app.js
@@ -0,0 +1,60 @@
+var stompClient = null;
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#greetings").html("");
+}
+
+function connect() {
+//端口是这个application的端口，也就是看application.properties中的设置，默认就是8080, 必须配置成localhost，而不能使用127.0.0.1，否则有跨域问题
+    //var socket = new SockJS('/websocket');
+
+    //var sockjs_url = 'http://localhost:8080/websocket';
+    //var sockjs_url = '/websocket';
+    var sockjs_url = 'http://127.0.0.1:6604/bullet';
+    //var sockjs_url = 'http://127.0.0.1:6605/websocket';
+    var socket = new SockJS(sockjs_url);
+    stompClient = Stomp.over(socket);
+    stompClient.connect({'AuthToken': 'yqbjtu'}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+
+        stompClient.subscribe('/topic/app01', function (greeting) {
+            console.log("subscribe edbb:" + greeting);
+            showGreeting(JSON.parse(greeting.body).content);
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendName() {
+    stompClient.send("/app/hello", {}, JSON.stringify({'name': $("#name").val()}));
+}
+
+function showGreeting(message) {
+    $("#greetings").append("<tr><td>" + message + "</td></tr>");
+}
+
+$(function () {
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    $( "#connect" ).click(function() { connect(); });
+    $( "#disconnect" ).click(function() { disconnect(); });
+    $( "#send" ).click(function() { sendName(); });
+});
+
